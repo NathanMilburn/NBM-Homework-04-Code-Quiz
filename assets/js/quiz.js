836ce01c8e0c3e6a3efc64aaf6cf7e1a598376b9
NathanM <nathan.milburn@outlook.com>
2022-01-15 17:02:48 -0800

    changed my header to a container and further customized my buttons and stylesheets + began work on my js sheet by creating a decrimenting timer that starts by clicking the 'start game' button

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,3 +6,60 @@
  * Use this data to populate your quiz questions, choices, and answers.
  */
 console.log(questions);
+
+var timerEl = document.querySelector('#countdown');
+var timer;
+// var timeLeft;
+// var mainEl = document.getElementById('main');
+
+// var startButton = document.querySelector("#start-button");
+
+// function startQuiz() {
+//     document.getElementById('timeLeft').style='color.green;';
+//     document.addEventListener(startButton);
+//     countDown();
+//   }
+
+// Correct Guess Goes HERE
+
+// Incorrect Guess Goes HERE
+
+// KEEP FOR LATER VVVVVVV
+
+// function countDown() {
+//     var timeLeft = 60;
+  
+//     var timeInterval = setInterval(function () {
+//       if (timeLeft > 1) {
+//         timerEl.textContent = timeLeft + ' seconds remaining';
+//         timeLeft--;
+//       } else if (timeLeft === 1) {
+//         timerEl.textContent = timeLeft + ' second remaining';
+//         timeLeft--;
+//       } else {
+//         timerEl.textContent = '';
+//         clearInterval(timeInterval);
+//       }
+//     }, 1000);
+//   }
+
+function countDown() {
+    var sec = 60;
+    clearInterval(timer);
+    timer = setInterval(function(){
+        sec--;
+        console.log(sec);
+        timerEl.textContent ='00:'+sec;
+
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+
+  const startBtn = document.querySelector('#start-button');
+
+  startBtn.addEventListener('click', () => {
+      countDown();
+  });
